d5de68238a642d37a511953bd554b814fd2147e5
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-02-22 12:42:55 +0530

    finalize mobile error update.

--- GoBunny/GoBunny.service.worker.js
+++ GoBunny/GoBunny.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771743883|3863043396';
+const CACHE_VERSION = '1771744354|4333867576';
 /** @type {string} */
 const CACHE_PREFIX = 'Cyborg bun-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
