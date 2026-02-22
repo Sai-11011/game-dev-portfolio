e13533e7c1d6b89255428395a718867929bf8dfc
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-02-22 12:28:22 +0530

    button error update in main.gd.

--- GoBunny/GoBunny.service.worker.js
+++ GoBunny/GoBunny.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771742385|2364901833';
+const CACHE_VERSION = '1771743462|3442345743';
 /** @type {string} */
 const CACHE_PREFIX = 'Cyborg bun-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
