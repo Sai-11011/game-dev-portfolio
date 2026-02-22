9141aae35c575289d03377f8cb8a1fbbf5e9eb28
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-02-22 12:10:13 +0530

    finalize mobile normal buttons.

--- GoBunny/GoBunny.service.worker.js
+++ GoBunny/GoBunny.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771741490|1469781724';
+const CACHE_VERSION = '1771742385|2364901833';
 /** @type {string} */
 const CACHE_PREFIX = 'Cyborg bun-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
