8155f2d3c8ab540fcd8ee282fac10b1926b2e683
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-02-22 12:35:08 +0530

    debugging tap.

--- GoBunny/GoBunny.service.worker.js
+++ GoBunny/GoBunny.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771743462|3442345743';
+const CACHE_VERSION = '1771743883|3863043396';
 /** @type {string} */
 const CACHE_PREFIX = 'Cyborg bun-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
