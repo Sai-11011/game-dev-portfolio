29d3e08ab9fc16cd9e6a973f208baba3c898d6eb
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-02-25 21:47:24 +0530

    Neon pong changed Square to circle

--- NeonPong/NeonPong.service.worker.js
+++ NeonPong/NeonPong.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771959584|796312873';
+const CACHE_VERSION = '1772036201|1771874004';
 /** @type {string} */
 const CACHE_PREFIX = 'Neon pong-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
