2aa9a2cc718440be62c13344c0fcb81464735f9c
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-03-03 15:12:59 +0530

    Space shooter ui update and Hanger logic complete.

--- SpaceShooter/SpaceShooter.service.worker.js
+++ SpaceShooter/SpaceShooter.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772167180|4038769449';
+const CACHE_VERSION = '1772530725|34073008';
 /** @type {string} */
 const CACHE_PREFIX = 'Space Shooter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
