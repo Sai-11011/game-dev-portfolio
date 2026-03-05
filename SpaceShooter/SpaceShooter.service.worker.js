0b59f80340f4d9d72e2028a2061dc3b05e3d911e
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-03-05 19:49:22 +0530

    shop is now working.

--- SpaceShooter/SpaceShooter.service.worker.js
+++ SpaceShooter/SpaceShooter.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772530725|34073008';
+const CACHE_VERSION = '1772720225|3384783906';
 /** @type {string} */
 const CACHE_PREFIX = 'Space Shooter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
