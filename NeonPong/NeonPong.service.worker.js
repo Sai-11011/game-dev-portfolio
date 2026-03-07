e698d75b4e654a8f3d7942545138b2e8cbbb0242
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-03-07 13:41:10 +0530

    all games updated.

--- NeonPong/NeonPong.service.worker.js
+++ NeonPong/NeonPong.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772036201|1771874004';
+const CACHE_VERSION = '1772870838|27394937';
 /** @type {string} */
 const CACHE_PREFIX = 'Neon pong-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
