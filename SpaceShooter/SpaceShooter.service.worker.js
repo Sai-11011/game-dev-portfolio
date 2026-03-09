9a9ced55e9e4b4c37dda1d8f36f51a59862b7bad
Sai-11011 <192403676+Sai-11011@users.noreply.github.com>
2026-03-09 16:11:08 +0530

    Updated waves after adding swamers.

--- SpaceShooter/SpaceShooter.service.worker.js
+++ SpaceShooter/SpaceShooter.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772993175|3391734627';
+const CACHE_VERSION = '1773052722|456982592';
 /** @type {string} */
 const CACHE_PREFIX = 'Space Shooter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
